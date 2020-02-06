42567991bd18c7fe92586157bc448b4f65d9d04a
aidanbek <aidanbek.k@gmail.com>
2020-02-07 00:12:55 +0600

    some fixes

--- resources/views/components/alert_message.blade.php
+++ resources/views/components/alert_message.blade.php
@@ -1,4 +1,4 @@
-<div class="alert alert-{{$messageType}} alert-dismissible fade show mt-3" role="alert" style="">
+<div class="alert alert-{{$messageType}} alert-dismissible fade show mt-3 alert-modal" role="alert">
     {{$message}}
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -7,16 +7,16 @@
 {{--        </ul>--}}
 {{--    </div>--}}
 {{--@endif--}}
-{{--<div class="container">--}}
-{{--    <div class="row">--}}
-{{--        <div class="col-sm-12">--}}
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
             @if(session('success'))
                 @include('components.alert_message', ['message' => session('success'), 'messageType' => 'success'])
             @endif
             @if(session('error'))
                 @include('components.alert_message', ['message' => session('error'), 'messageType' => 'error'])
             @endif
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
+        </div>
+    </div>
+</div>
 
