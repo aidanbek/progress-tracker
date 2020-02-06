7b982bafab94fb8f6fce2bd85e5aaecdd86bd258
aidanbek <aidanbek.k@gmail.com>
2020-02-07 00:50:57 +0600

    some refactoring

--- public/js/app.js
+++ public/js/app.js
@@ -9,9 +9,6 @@ $(document).ready(function () {
         const modalId = $(this).data('target');
 
         if (focusableModalInputs.hasOwnProperty(modalId)) {
-            console.log(`has prop ${modalId}`);
-            console.log(focusableModalInputs[modalId]);
-
             $(modalId).on('shown.bs.modal', function (e) {
                 console.log('event fired');
                 $(modalId).find(focusableModalInputs[modalId]).focus();
